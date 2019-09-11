2252f566c5ae0d741a7502c8b6aafe78edca9aca
Ed <edwoulfe@gmail.com>
2019-09-11 09:26:39 -0500

    Update async-and-await.js

--- async-and-await.js
+++ async-and-await.js
@@ -1,29 +1,32 @@
 // Async and Await
 
- function doSomething(){
-        console.log(1);
-     fetch("https://jsonplaceholder.typicode.com/todos/1")
-        .then(response => response.json())
-        .then(json => console.log(json));
-         .catch((error) => console.log(error));
-            console.log(2);
- }
+// function doSomething() {
+//   console.log(1);
+//   fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then(response => response.json())
+//     .then(json => console.log(json));
+
+//   console.log(2);
+// }
 
 // 1
 // 2
 // user data
 
-async function doSomething(){
-    console.log(1);
-    try{
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+async function doSomething() {
+  console.log(1);
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
     const json = await response.json();
     console.log(json);
-    }catch(e){
-        console.log(e)
-    }
-    console.log(2);
+  } catch (e) {
+    console.log(e);
+  }
+  console.log(2);
 }
+doSomething();
 // await = waits for the fetch to continue
 // 1
 // user data
